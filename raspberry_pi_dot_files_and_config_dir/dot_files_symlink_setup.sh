57b718e6343faaaed25f0578402925257472833e
manish516 <manish516@outlook.com>
2023-09-21 17:35:41 +0530

    setup scrpit
    to setup symlink from git repo to pi's home folder

---
+++ raspberry_pi_dot_files_and_config_dir/dot_files_symlink_setup.sh
@@ -0,0 +1,19 @@
+# run this scrpit without sudo --> bash scrpit_name.sh
+
+folder_name=git_repo
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/dot_files_in_home_dir/.vim $HOME/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/dot_files_in_home_dir/.vimrc $HOME/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/dot_files_in_home_dir/.tmux.conf $HOME/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/dot_files_in_home_dir/.alias $HOME/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/dot_files_in_home_dir/.bashrc $HOME/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/dot_config/lf $HOME/.config/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/go $HOME/
+
+ln -s $HOME/$folder_name/my_pi_basic_sys_git_repo/raspberry_pi_dot_files_and_config_dir/pi_scripts $HOME/
